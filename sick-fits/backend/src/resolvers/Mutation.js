830f8db60b9f55bc0eaab3822976bc7488a84c71
Vitor Correia <vitorbaziliogcorreia@gmail.com>
2021-01-12 22:09:36 -0500

    Edit a product

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -7,7 +7,22 @@ const Mutations = {
         }, info);
 
         return item
-    }
+    },
+    updateItem(parent, args, ctx, info) {
+        //take a copy of the updates
+        const updates = { ...args };
+        //remove the ID from the updates
+        delete updates.id;
+        //run the update method
+        return ctx.db.mutation.updateItem({
+            data: updates,
+            where: {
+                id: args.id
+            },
+        },
+            info
+        );
+    },
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -6,8 +6,8 @@ import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
 import Error from './ErrorMessage';
 
-const CREATE_ITEN_MUTATION = gql`
-    mutation CREATE_ITEN_MUTATION(
+const CREATE_ITEM_MUTATION = gql`
+    mutation CREATE_ITEM_MUTATION(
         $title: String!
         $description: String!
         $price: Int!
@@ -34,14 +34,34 @@ class CreateItem extends Component {
         largeImage: '',
         price: 0,
     };
+
     handleChange = e => {
         const { name, type, value } = e.target;
         const val = type === 'number' ? parseFloat(value) : value;
         this.setState({ [name]: val });
     };
+
+    uploadFile = async e => {
+        console.log('Uploading files');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits');
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/diwbkabgh/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log('file', file);
+        this.setState({
+            image: file.secure_url,
+            largeImage: file.eager[0].secure_url
+        });
+    };
     render() {
         return (
-            <Mutation mutation={CREATE_ITEN_MUTATION} variables={this.state}>
+            <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
                 {(createItem, { loading, error }) => (
                     <Form onSubmit={async e => {
                         e.preventDefault();
@@ -54,9 +74,21 @@ class CreateItem extends Component {
                     }}>
                         <Error error={error} />
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="file">
+                                Image
+                                <input
+                                    type="file"
+                                    id="file"
+                                    name="file"
+                                    placeholder="Upload an Image"
+                                    required
+                                    onChange={this.uploadFile}
+                                />
+                                {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
+                            </label>
                             <label htmlFor="title">
                                 Title
-                        <input
+                                <input
                                     type="text"
                                     id="title"
                                     name="title"
@@ -101,4 +133,4 @@ class CreateItem extends Component {
 }
 
 export default CreateItem;
-export { CREATE_ITEN_MUTATION };
+export { CREATE_ITEM_MUTATION };
